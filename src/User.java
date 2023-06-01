7d87be4c92a61c4166ebcc6c61e8062b20ba6cea
leesumin <daram015812@naver.com>
2023-06-02 00:18:03 +0900

    initialize db method 수정
    user database 초기화할 때 다른 두 db도 초기화하도록 수정, 데이터베이스명과 이미지파일 절대경로 수정 필요

--- src/User.java
+++ src/User.java
@@ -4,6 +4,9 @@ public class User {
     private static final String DB_URL = "jdbc:mysql://localhost:3306/User";
     private static final String DB_USERNAME = "dajeong";
     private static final String DB_PASSWORD = "6545";
+    
+    // 이미지 파일이 위치한 절대경로
+    private static final String IMG_PATH = "C:\\Users\\daram\\eclipse-workspace\\ShoeMarket\\";
 
     private String name;
     private String id;
@@ -39,6 +42,98 @@ public class User {
         } catch (SQLException e) {
             e.printStackTrace();
         }
+        
+        Connection conn;
+    	Statement stmt;
+    	String DB_NAME = "shoe_mart"; // 사용하시는 데이터베이스명으로 변경해주세요
+    	PreparedStatement psmt;
+    	
+    	try {
+    		Class.forName("com.mysql.jdbc.Driver");
+    		conn = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+    		
+    		stmt = conn.createStatement();
+    		
+    		String checkQuery = "SELECT count(*) FROM information_schema.tables WHERE table_schema = '"+DB_NAME+"' AND table_name='products'";
+			Statement stmt2 = conn.createStatement();
+			ResultSet rs = stmt2.executeQuery(checkQuery);
+			rs.next();
+
+			if (rs.getInt(1) == 0) {
+
+				String createQuery = "CREATE TABLE products (" + "pId int primary key auto_increment, "
+						+ "pName varchar(30) not null, " + "price int not null, " + "cnt int default 0, "
+						+ "img longblob)";
+
+				stmt.executeUpdate(createQuery);
+
+				Product[] products = new Product[8];
+				String[] file = new String[8];
+
+				products[0] = new Product("썬 여성 샌들 젤리슈즈", 17900, 10, null);
+				products[1] = new Product("코트 버로우 로우2", 59000, 10, null);
+				products[2] = new Product("아딜렛22", 46000, 10, null);
+				products[3] = new Product("척테일러 올스타 클래식", 55000, 10, null);
+				products[4] = new Product("클래식 클로그", 49000, 10, null);
+				products[5] = new Product("메이린 여성 홀스빗 로퍼", 39900, 10, null);
+				products[6] = new Product("피카츄 포켓 장화", 9900, 10, null);
+				products[7] = new Product("헬로카봇 타운 장화", 8900, 10, null);
+				
+				file[0] = "썬여성샌들젤리슈즈.png";
+				file[1] = "코트버로우로우2.png";
+				file[2] = "아딜렛22.png";
+				file[3] = "척테일러올스타클래식.png";
+				file[4] = "클래식클로그.png";
+				file[5] = "메이린여성홀스빗로퍼.png";
+				file[6] = "피카츄포켓장화.png";
+				file[7] = "헬로카봇타운장화.png";
+				
+				for (int i=0; i<8; i++) {
+					String psmtStr = "INSERT INTO products values (null, ?, ?, ?, ?)";
+					
+					File f = new File(IMG_PATH+file[i]);
+					FileInputStream fis = new FileInputStream(f);
+										
+					psmt = conn.prepareStatement(psmtStr);
+					
+					psmt.setString(1, products[i].getpName());
+					psmt.setInt(2, products[i].getPrice());
+					psmt.setInt(3, products[i].getCnt());
+					psmt.setBinaryStream(4, fis,(int)f.length());
+									
+					psmt.executeUpdate();
+					fis.close();
+					psmt.close();
+				}
+			}
+			rs.close(); stmt2.close(); stmt.close(); conn.close();
+		} catch (Exception e) {
+    		e.printStackTrace();
+    	}
+    	
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+			
+			stmt = conn.createStatement();
+			
+			String createQuery = "CREATE TABLE IF NOT EXISTS cart ("+
+					"cart_id int primary key auto_increment, "+
+					"user_id varchar(50), "+
+					"product_id int, "+
+					"amount int default 0, "+
+					"foreign key(user_id) references users(id), "+
+					"foreign key(product_id) references products(pId))";
+			
+			System.out.println(createQuery);
+			
+			stmt.executeUpdate(createQuery);
+			
+			stmt.close(); conn.close();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
     }
 
 

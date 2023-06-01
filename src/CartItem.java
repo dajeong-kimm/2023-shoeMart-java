aff42e8c4a19a022e213bfdf423a0721d3bd4d65
leesumin <daram015812@naver.com>
2023-06-01 18:10:44 +0900

    Update CartItem.java
    cart item 필드 변수로 user, pId(product id) 추가, price, img cartitems 삭제
    addCart 함수로 장바구니에 추가하면 데이터베이스에 업데이트되는 것으로 변경

--- src/CartItem.java
+++ src/CartItem.java
@@ -1,56 +1,72 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.sql.*;
+
 
 public class CartItem {
-    private Product product;
+	
+	private static final String DB_URL = "jdbc:mysql://localhost:3306/shoe_mart";
+    private static final String DB_USERNAME = "root";
+    private static final String DB_PASSWORD = "!!dltnals!!6280";
+    
+    private User user;
+    private int pId;
     private int quantity;
-    private int price;
-    private String img;
-    private static List<CartItem> cartItems = new ArrayList<>();
 
-    public CartItem(Product product, int quantity, int price, String img) {
-        this.product = product;
+    public CartItem(User user, int pId, int quantity) {
+    	this.user = user;
+        this.pId = pId;
         this.quantity = quantity;
-        this.price = price;
-        this.img = img;
     }
-
-    public Product getProduct() {
-        return product;
+    
+    public User getUser() {
+    	return this.user;
     }
-
+    
+    public int getPId() {
+    	return this.pId;
+    }
+    
     public int getQuantity() {
-        return quantity;
+    	return this.quantity;
     }
+    
+    public void addCart() {
+    	 Connection con;
+         
+         try {
+         	Class.forName("com.mysql.jdbc.Driver");
+     	
+         	con = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+         	
+         	Statement stmt = con.createStatement();
+         	ResultSet rs;
+         	PreparedStatement psmt;
+         	
+         	String sql = "SELECT * from cart WHERE user_id='"+this.user.getId()+"' and product_id="+pId;
+         	        	
+         	rs = stmt.executeQuery(sql);
+         	
+         	if (rs.next()) {
+         		int cnt = rs.getInt(3);
+         		
+         		cnt += quantity;
+         		
+         		sql = "UPDATE cart SET amount="+cnt+" WHERE user_id='"+this.user.getId()+"' and product_id="+pId;
+         		stmt.executeUpdate(sql);
+         	}
+         	else {
+         		psmt = con.prepareStatement("INSERT INTO cart VALUES(null, ?, ?, ?)");
+         	
+         		psmt.setString(1, this.user.getId());
+         		psmt.setInt(2, this.pId);
+         		psmt.setInt(3, this.quantity);
+     	
+         		psmt.executeUpdate();
+         		psmt.close();
+         	}
+         	rs.close(); stmt.close(); con.close();
+         } catch(Exception e) {
+         	e.printStackTrace();
+         }
 
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
-    }
-
-    public int getPrice() {
-        return price;
-    }
-
-    public String getImg() {
-        return img;
-    }
-
-    public static List<CartItem> getCartItems() {
-        return cartItems;
-    }
-
-    public static void addToCart(Product product) {
-        boolean found = false;
-        for (CartItem item : cartItems) {
-            if (item.getProduct().equals(product)) {
-                item.setQuantity(item.getQuantity() + 1);
-                found = true;
-                break;
-            }
-        }
-        if (!found) {
-            CartItem newItem = new CartItem(product, 1, product.getPrice(), product.getImg());
-            cartItems.add(newItem);
-        }
     }
 }

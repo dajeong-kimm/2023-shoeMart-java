eb101bae0fe90f825bb64cbb8a85673c70b8769b
leesumin <daram015812@naver.com>
2023-06-03 15:23:29 +0900

    Update CartPage.java
    장바구니에서 물건 취소 시 새창 없이 바로 반영되도록 수정

--- src/CartPage.java
+++ src/CartPage.java
@@ -12,16 +12,82 @@ public class CartPage extends JFrame {
 	private static final long serialVersionUID = 1L;
 	private User user;
 	private HashMap<Integer, Product> prod = new HashMap<>();
-	private List<CartItem> cartItems = new ArrayList(); // Change the type to List<CartItem>
-	private JLabel totalPriceLabel;
+	private List<CartItem> cartItems; // Change the type to List<CartItem>
 
 	private static final String DB_URL = "jdbc:mysql://localhost:3306/shoe_mart";
 	private static final String DB_USERNAME = "root";
 	private static final String DB_PASSWORD = "!!dltnals!!6280";
+	
+	private JPanel panel;
+	private JPanel centerPanel;
+	private JPanel totalPanel;
+	private JScrollPane scrollPane;
+
 
 	public CartPage(User user) { // Update the constructor
 		this.user = user;
 
+		loadCart();
+				
+		// 장바구니 상품 목록
+		setTitle("Cart");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(900, 600);
+		setResizable(false);
+		setLocationRelativeTo(null);
+
+		panel = new JPanel();
+		panel.setLayout(new BorderLayout());
+		
+		centerPanel = center();
+		scrollPane = new JScrollPane(centerPanel, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+		
+		totalPanel = total();
+
+		panel.add(scrollPane, BorderLayout.CENTER);
+		panel.add(totalPanel, BorderLayout.NORTH);
+
+
+		JPanel bottomPanel = new JPanel();
+		bottomPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
+
+		JButton backButton = new JButton("상품 페이지");
+		JButton mypageButton = new JButton("마이페이지");
+		JButton payButton = new JButton("결제하기");
+
+		bottomPanel.add(backButton);
+		bottomPanel.add(mypageButton);
+		bottomPanel.add(payButton);
+		backButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				dispose(); // 현재 페이지(MainPage) 종료
+				MainPage mainPage = new MainPage(user, cartItems); // MyPage 클래스로 이동
+			}
+		});
+
+		mypageButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				dispose(); // 현재 페이지(MainPage) 종료
+				MyPage myPage = new MyPage(user); // MyPage 클래스로 이동
+			}
+		});
+
+		payButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				dispose(); // 현재 페이지(MainPage) 종료
+				PayPage payPage = new PayPage(user, cartItems); // MyPage 클래스로 이동
+			}
+		});
+
+		panel.add(bottomPanel, BorderLayout.SOUTH);
+
+		add(panel);
+		setVisible(true);
+
+	}
+	
+	private void loadCart() {
+		cartItems = new ArrayList();
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 			Connection conn = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
@@ -61,36 +127,26 @@ public class CartPage extends JFrame {
 			e.printStackTrace();
 		}
 
-		// 장바구니 상품 목록
-		setTitle("Cart");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setSize(900, 600);
-		setResizable(false);
-		setLocationRelativeTo(null);
-
-		JPanel panel = new JPanel();
-		panel.setLayout(new BorderLayout());
-
-		
+	}
+	
+	private JPanel center() {
 		if (cartItems.isEmpty()) {
 			JPanel printPanel = new JPanel();
 			JLabel printLabel = new JLabel("장바구니 목록이 비었습니다.");
-			
+
 			printPanel.add(printLabel);
-			panel.add(printPanel, BorderLayout.CENTER);
+			
+			return printPanel;
 		}
-
+		
 		else {
-
 			JPanel cartItemsPanel = new JPanel();
-			JScrollPane scrollPane = new JScrollPane(cartItemsPanel, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
-					JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
 			cartItemsPanel.setLayout(new GridLayout(cartItems.size(), 4, 30, 30));
-
+			
 			int num = cartItems.size();
 
 			Insets inset = new Insets((int) 240 / (num * num), 40, (int) 240 / (num * num), 40);
-
+			
 			for (CartItem item : cartItems) { // Update the loop variable type
 				Product product = prod.get(item.getPId());
 				JLabel nameLabel = new JLabel(product.getpName()); // Access the product from the CartItem
@@ -119,58 +175,35 @@ public class CartPage extends JFrame {
 				cancelButton.addActionListener(new ActionListener() {
 					public void actionPerformed(ActionEvent e) {
 						removeFromCart(item); // Pass the product to removeFromCart()
-						setVisible(false);
-						CartPage cartpage = new CartPage(user);
+						panel.remove(scrollPane);
+						panel.remove(totalPanel);
+						
+						loadCart();
+						
+						centerPanel = center();
+						scrollPane = new JScrollPane(centerPanel, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+						totalPanel = total();
+						panel.add(scrollPane, BorderLayout.CENTER);
+						panel.add(totalPanel, BorderLayout.NORTH);
+						panel.revalidate();
+						panel.repaint();
 					}
 				});
 			}
 
-			JPanel totalPanel = new JPanel();
-			totalPriceLabel = new JLabel("총 가격: " + Integer.toString(calculateTotalPrice()));
-
-			totalPanel.add(totalPriceLabel);
-
-			panel.add(scrollPane, BorderLayout.CENTER);
-			panel.add(totalPanel, BorderLayout.NORTH);
 			
+			return cartItemsPanel;
 		}
+	}
+	
+	private JPanel total() {
+		JPanel total = new JPanel();
+
+		JLabel totalPriceLabel = new JLabel("총 가격: " + Integer.toString(calculateTotalPrice()));
+
+		total.add(totalPriceLabel);
 		
-		JPanel bottomPanel = new JPanel();
-        bottomPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
-
-        JButton backButton = new JButton("상품 페이지");
-        JButton mypageButton = new JButton("마이페이지");
-        JButton payButton = new JButton("결제하기");
-
-        bottomPanel.add(backButton);
-        bottomPanel.add(mypageButton);
-        bottomPanel.add(payButton);
-        backButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                dispose(); // 현재 페이지(MainPage) 종료
-                MainPage mainPage = new MainPage(user, cartItems); // MyPage 클래스로 이동
-            }
-        });
-        
-        mypageButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                dispose(); // 현재 페이지(MainPage) 종료
-                MyPage myPage = new MyPage(user); // MyPage 클래스로 이동
-            }
-        });
-        
-        payButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                dispose(); // 현재 페이지(MainPage) 종료
-                PayPage payPage = new PayPage(user, cartItems); // MyPage 클래스로 이동
-            }
-        });
-        
-		panel.add(bottomPanel, BorderLayout.SOUTH);
-        
-		add(panel);
-		setVisible(true);
-
+		return total;
 	}
 
 	// 장바구니에서 상품 제외
@@ -181,7 +214,8 @@ public class CartPage extends JFrame {
 			Connection conn = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
 
 			Statement stmt = conn.createStatement();
-			stmt.executeUpdate("DELETE FROM cart WHERE user_id='" + this.user.getId() + "' and product_id=" + item.getPId());
+			stmt.executeUpdate(
+					"DELETE FROM cart WHERE user_id='" + this.user.getId() + "' and product_id=" + item.getPId());
 
 			stmt.close();
 			conn.close();
@@ -192,7 +226,6 @@ public class CartPage extends JFrame {
 		cartItems.remove(item);
 		prod.remove(item.getPId());
 
-		totalPriceLabel.setText("총 가격: " + calculateTotalPrice());
 		JOptionPane.showMessageDialog(CartPage.this, "상품이 장바구니에서 제외되었습니다.");
 	}
 
